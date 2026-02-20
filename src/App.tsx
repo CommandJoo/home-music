4cb243796bcf1ce82e661ffd6d4db922090ac858
CommandJoo <hansjohannes118@gmail.com>
2026-02-20 13:22:52 +0100

    add music provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,68 @@
 import './App.css'
-import {useEffect, useState} from "react";
+import {createContext, type ReactNode, useEffect, useState} from "react";
 import Audio from "./app/audio/Audio.tsx";
 import Sidebar from "./app/sidebar/Sidebar.tsx";
 import type {Collection, PlayItem, Recording, Song, User, Users} from "./app/types.ts";
 import Searchbar from "./app/searchbar/Searchbar.tsx";
 
-type MusicProviderType = {
+type MusicContextType = {
     db: Song[];
     reloadSongs: () => void;
     page?: Collection;
-    changePage: (page: Collection) => void;
-    playing: Song;
+    changePage: (page: PlayItem|null) => void;
+    playing?: Song;
     setPlaying: (playing: Song) => void;
 
     users: Users;
     refreshUsers: () => void;
-    currentUser: User;
+    currentUser?: User;
+}
+const MusicContext = createContext<MusicContextType | null>(null);
+
+export function MusicProvider({ children }: {children: ReactNode}) {
+    const [db, setDb] = useState<Song[]>([]);
+    const [page, setPage] = useState<Collection|undefined>();
+    const [playing, setPlaying] = useState<Song|undefined>();
+    const [users, setUsers] = useState<Users>({current_user: "", users: []});
+    const [currentUser, setCurrentUser] = useState<User>();
+
+    async function reloadSongs() {
+        const response = await fetch("/api/songs");
+        const json = await response.json() as Song[];
+        setDb(json);
+    }
+
+    function changePage(item: PlayItem|null) {
+        if(!item) {
+            setPage(undefined);
+            return;
+        }
+        if (item.type === "collection") {
+            setPage(item.item as Collection)
+        }
+    }
+
+    async function refreshUsers() {
+        const response = await fetch("/api/users");
+        const data = await response.json() as Users;
+        setUsers(data);
+
+        const currentId = data.current_user;
+        if(currentId && currentId.length > 0) {
+            for (const user of data.users) {
+                if(user.id===currentId) {
+                    const currentResponse = await fetch(user.path);
+                    const currentData = await currentResponse.json() as User;
+                    setCurrentUser(currentData);
+                    break;
+                }
+            }
+        }
+    }
+
+    return <MusicContext.Provider value={{db, reloadSongs, page, changePage, playing, setPlaying, users, refreshUsers, currentUser}}>
+        {children}
+    </MusicContext.Provider>
 }
 
 function App() {
