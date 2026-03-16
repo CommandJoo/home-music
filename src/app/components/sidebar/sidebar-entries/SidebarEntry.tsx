7728935fab575d6bca2bc466fd70e164799485dd
CommandJoo <hansjohannes118@gmail.com>
2026-03-16 19:42:51 +0100

    Changed UserMenu to use ContextMenu system, also added icons to the entries

--- src/app/components/sidebar/sidebar-entries/SidebarEntry.tsx
+++ src/app/components/sidebar/sidebar-entries/SidebarEntry.tsx
@@ -5,14 +5,16 @@ import {type CSSProperties, type ReactElement} from "react";
 type SidebarEntryProps = {
     preview?: ReactElement
     className?: string
-    onClick?: () => void
+    onClick?: (e?: React.MouseEvent) => void
     onContext?: (e: React.MouseEvent) => void
     style?: CSSProperties
     key?: number
 }
 
 export default function SidebarEntry(props: SidebarEntryProps) {
-    return <div id={"sidebar-entry"} style={props.style} className={props.className} onClick={props.onClick}
+    return <div id={"sidebar-entry"} style={props.style} className={props.className} onClick={(e) => {
+        if (props.onClick) props.onClick(e)
+    }}
                 onContextMenu={(e) => {
                     if (props.onContext) props.onContext(e);
                 }}>
--- src/app/components/sidebar/sidebar-entries/UserSidebarEntry.tsx
+++ src/app/components/sidebar/sidebar-entries/UserSidebarEntry.tsx
@@ -1,9 +1,12 @@
 import "./UserSidebarEntry.css"
 import type {User} from "../../../types.ts";
 import SidebarEntry from "./SidebarEntry.tsx";
-import {type CSSProperties, useState} from "react";
+import {type CSSProperties} from "react";
 import {useMusic} from "../../../../providers/MusicProvider.tsx";
 import {stringToColor} from "../../../util.ts";
+import {useContextMenu} from "../../../../providers/ContextMenuProvider.tsx";
+import ContextMenuButton from "../../../context-menu/ContextMenuButton.tsx";
+import {TbPlaylistAdd, TbSettings, TbUserPlus} from "react-icons/tb";
 
 function picture(user: User) {
     if (user.picture.length > 0) {
@@ -14,48 +17,68 @@ function picture(user: User) {
     return <h1 style={{"--color": stringToColor(user.name)} as CSSProperties}>{user.name.charAt(0).toUpperCase()}</h1>;
 }
 
+function UserMenu({hasUser}: { hasUser: boolean }) {
+    const {changePage} = useMusic();
+    const {close} = useContextMenu();
+
+    if (hasUser) {
+        return <div id={"user-menu"}>
+            <ContextMenuButton className={"button"} onClick={() => {
+                changePage("settings", "general");
+                close();
+            }} icon={<TbSettings className={"icon"}/>}>Settings
+            </ContextMenuButton>
+            <ContextMenuButton icon={<TbPlaylistAdd className={"icon"}/>} className={"button"} onClick={() => {
+                changePage("create_playlist");
+                close();
+            }}>
+                Create Playlist
+            </ContextMenuButton>
+        </div>;
+    } else {
+        return <div id={"user-menu"}>
+            <ContextMenuButton icon={<TbUserPlus className={"icon"}/>} className={"button"} onClick={() => {
+                changePage("settings", "profile");
+                close();
+            }}>Create Profile
+            </ContextMenuButton>
+        </div>;
+    }
+
+}
+
 export default function UserSidebarEntry() {
-    const {currentUser, changePage} = useMusic();
-    const [open, setOpen] = useState(false);
+    const {currentUser} = useMusic();
+    const {open} = useContextMenu();
 
     if (!currentUser) {
-        if (!open) {
-            return <SidebarEntry onClick={() => setOpen(!open)} className={"user none"} preview={<h1>x</h1>}>
-            </SidebarEntry>;
-        }
         return <div id={"user-with-menu"}>
-            <SidebarEntry onClick={() => setOpen(!open)} className={"user none"} preview={<h1>x</h1>}>
+            <SidebarEntry onClick={(e) => {
+                if (e) {
+                    e.stopPropagation();
+                    const rect = e.currentTarget.getBoundingClientRect();
+                    open(
+                        rect.left,
+                        rect.bottom,
+                        <UserMenu hasUser={false}/>);
+                }
+            }} className={"user none"} preview={<h1>x</h1>}>
             </SidebarEntry>
-            <div id={"user-menu"}>
-                <div className={"button"} onClick={() => {
-                    changePage("settings", "profile");
-                    setOpen(false);
-                }}>Create Profile
-                </div>
-            </div>
+
         </div>;
-    } else if (currentUser) {
-        if (!open) {
-            return <SidebarEntry onClick={() => setOpen(!open)} className={"user selected"}
-                                 preview={picture(currentUser)}>
-            </SidebarEntry>;
-        }
+    } else {
         return <div id={"user-with-menu"}>
-            <SidebarEntry onClick={() => setOpen(!open)} className={"user selected"} preview={picture(currentUser)}>
+            <SidebarEntry onClick={(e) => {
+                if (e) {
+                    e.stopPropagation();
+                    const rect = e.currentTarget.getBoundingClientRect();
+                    open(
+                        rect.left,
+                        rect.bottom,
+                        <UserMenu hasUser={true}/>);
+                }
+            }} className={"user selected"} preview={picture(currentUser)}>
             </SidebarEntry>
-            <div id={"user-menu"}>
-                <div className={"button"} onClick={() => {
-                    changePage("settings", "general");
-                    setOpen(false);
-                }}>Settings
-                </div>
-                <div className={"button"} onClick={() => {
-                    changePage("create_playlist");
-                    setOpen(false);
-                }}>
-                    Create Playlist
-                </div>
-            </div>
         </div>;
     }
 }
